93b6e43d5ca4394dea90b8ec64a89ce448282fad
Alisa Porras <aljuporras@gmail.com>
2023-03-21 21:53:33 -0400

    1st commit: under helloworld

---
+++ TestGit2.java
@@ -0,0 +1,8 @@
+public class TestGit2
+{//class beg
+
+    public static void main(String[] args)
+    {//main beg
+        System.out.println("1st commit: git int in helloworld dir");
+    }//main end
+}//class end

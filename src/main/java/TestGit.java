25f4a3372f3d9fcae94d86ff24a44c24d5b79308
Alisa Porras <aljuporras@gmail.com>
2023-03-22 00:13:51 -0400

    this is my 1st commit for second homework

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -1,10 +1,55 @@
+import java.util.Scanner;
+
 public class TestGit
 {
     public static void main(String[] args)
     {
-        System.out.println("hello github");
-        System.out.println("hello github2");
-        System.out.println("hello github3");
-        System.out.println("hello github4");
+      //how user input info
+      Scanner scannerInput1  = new Scanner(System.in);
+
+        System.out.print("Enter your full name:");
+        String nameInput = scannerInput1.nextLine();
+        System.out.print("name length: ");
+        int nameLength = nameInput.length();
+        double nameEvenOdd;
+        nameEvenOdd = nameLength % 2;
+        if (nameEvenOdd == 0)
+        {
+            System.out.println("name string is an even number");
+        }
+        else
+        {
+            System.out.println("name string is an odd number");
+        }
+        System.out.println(nameLength);
+
+
+
+        System.out.print("Enter your address:");
+        String addressInput = scannerInput1.nextLine();
+        System.out.print("address length: ");
+        int addressLength = addressInput.length();
+        double addressEvenOdd;
+        addressEvenOdd = addressLength % 2;
+        if (addressEvenOdd == 0)
+        {
+            System.out.println("address string is an even number");
+        }
+        else
+        {
+            System.out.println("address string is an odd number");
+        }
+        System.out.println(addressLength);
+
+
+        System.out.println("thank you");
+
+
+
+
+        //System.out.println("hello github");
+        //System.out.println("hello github2");
+        //System.out.println("hello github3");
+        //System.out.println("hello github4");
     }
 }
